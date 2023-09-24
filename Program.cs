6931b76e32c11cc2bb1d9ce0de424ef2c5ca3bb1
jensencarr <karlmartenssonsales@gmail.com>
2023-09-24 17:48:12 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv: ");
+            string input = Console.ReadLine();
+            string konsunanter = "bcdfghjklmnpqrstvwxz";
+
+
+            string rövarspråket = "";
+
+            foreach (var bokstav in input)
+            {
+                
+                if (konsunanter.Contains(bokstav))
+                {
+                    rövarspråket += bokstav + "o" + bokstav;
+                    
+                }
+                else
+                {
+                    rövarspråket += bokstav;
+                }
+            }
+            Console.Write(rövarspråket);
+
+        }
+    }
+}
